9dd3dc1ca9b1b8e5ed2c24186c57e856a194881a
곽유나 <yoona405@konkuk.ac.kr>
2024-05-16 16:50:30 +0900

    refactor : message패키지 lombok을 활용한 생성자 제거

--- cm/src/main/java/distributed/cm/common/message/DefaultMessage.java
+++ cm/src/main/java/distributed/cm/common/message/DefaultMessage.java
@@ -2,28 +2,15 @@ package distributed.cm.common.message;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import distributed.cm.common.domain.User;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
-@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class DefaultMessage implements Message{
     private int messageType;
-
     private int entry;
-
     private int drawType;
-
     private String userId;
-
     private User usr;
-
-    public DefaultMessage(int messageType, int entry){
-        this.messageType = messageType;
-        this.entry = entry;
-    }
-
-    public DefaultMessage() {
-    }
 }
--- cm/src/main/java/distributed/cm/common/message/SquareMessage.java
+++ cm/src/main/java/distributed/cm/common/message/SquareMessage.java
@@ -1,22 +1,13 @@
 package distributed.cm.common.message;
 
 import distributed.cm.common.domain.Square;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
-@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class SquareMessage implements DrawMessage{
     private int messageType;
     private int drawType;
     private Square draw;
-
-    public SquareMessage() {
-    }
-
-    public SquareMessage(int messageType, int drawType, Square draw) {
-        this.messageType = messageType;
-        this.drawType = drawType;
-        this.draw = draw;
-    }
 }
